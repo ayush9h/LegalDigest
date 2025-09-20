805b5cb614f2c98ca2b87bc11ae0c160ae0dfe87
Ayush <ayush.kumar.21cse@bmu.edu.in>
2025-09-20 13:14:24 +0530

    added visualization code

---
+++ logs/vis.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+df = pd.read_csv("training_log.csv")
+
+plt.figure(figsize=(15, 8))
+plt.plot(df["epoch"], df["loss"], "bo")
+plt.xlabel("Number of epochs", loc="center")
+plt.ylabel("Loss", loc="center")
+
+plt.savefig("training_loss.png")
